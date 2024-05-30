e3e0c0c6976818ec220b4784a87ffb5df971ca07
Yuri <yurisamp123@gmail.com>
2024-05-30 18:33:49 -0300

    chore: move read languages to other file

--- args/args.go
+++ args/args.go
@@ -4,21 +4,10 @@ import (
 	"Yomu/output"
 	"Yomu/reader"
 	"Yomu/utils"
-	"encoding/json"
 	"fmt"
 	"os"
 )
 
-type Languages map[string] Language
-
-type Language struct {
-	Name               string   `json:"name"`
-  LineComment       []string `json:"line_comment,omitempty"`
-  MultiLineComments []string `json:"multi_line_comments,omitempty"`
-  Extensions        []string `json:"extensions,omitempty"`
-}
-
-var supportedLanguages Languages
 
 func HandleArgs() {
 	version := "0.0.1"
@@ -35,7 +24,7 @@ func HandleArgs() {
 	}
 
 	if utils.Includes(args, "-l") || utils.Includes(args, "--languages") {
-		readLanguages()
+		reader.ReadLanguages()
 		os.Exit(0)
 	}
 
@@ -49,25 +38,4 @@ func HandleArgs() {
 	info := reader.Start(dir)
 		
 	output.OutputBuilder(info)
-}
-
-
-func readLanguages() {
-
-	jsonFile, err := os.ReadFile("languages.json")
-
-	if err != nil {
-		panic(err)
-	}
-
-	err = json.Unmarshal(jsonFile, &supportedLanguages)
-
-	
-	if err != nil {
-		panic(err)
-	}
-	
-	for _, language := range supportedLanguages {
-		fmt.Println(language.Name)
-	}
 }
